360af97335027650254f3b47eedcd0f38f5febaf
Emiya <subaru.xia@zoho.com>
2018-10-31 14:29:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,7 +50,7 @@ sed -i -e "s@^ipv4-network =.*@ipv4-network = ${VPN_NETWORK}@" \
 	-e "s@^default-domain =.*@default-domain = ${VPN_DOMAIN}@" \
 	-e "s@^ipv4-netmask =.*@ipv4-netmask = ${VPN_NETMASK}@" $CONFIG_FILE
 changeConfig "tcp-port" "$PORT"
-changeConfig "udp-port" "$PORT"
+#changeConfig "udp-port" "$PORT"
 
 # Config V2Ray
 sed -i "s/d.c.b.a/${V2RAY_SERVER}/g" /etc/v2ray/config.json

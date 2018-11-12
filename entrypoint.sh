bcc1293013a856f759a8825cf7b76c3d10568b2e
Emiya <subaru.xia@zoho.com>
2018-11-12 18:06:40 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,6 +68,20 @@ iptables -t nat -A POSTROUTING -s ${VPN_NETWORK}/${VPN_NETMASK} -j MASQUERADE
 # 创建 V2Ray NAT 表
 iptables -t nat -N V2RAY
 iptables -t nat -A PREROUTING -s ${VPN_NETWORK}/${VPN_NETMASK} -p tcp -j V2RAY
+# 不转发内网网段
+iptables -t nat -A V2RAY -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A V2RAY -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A V2RAY -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A V2RAY -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A V2RAY -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A V2RAY -d 192.0.0.0/24 -j RETURN
+iptables -t nat -A V2RAY -d 192.0.2.0/24 -j RETURN
+iptables -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A V2RAY -d 198.18.0.0/15 -j RETURN
+iptables -t nat -A V2RAY -d 198.51.100.0/24 -j RETURN
+iptables -t nat -A V2RAY -d 203.0.113.0/24 -j RETURN
+iptables -t nat -A V2RAY -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A V2RAY -d 240.0.0.0/4 -j RETURN
 # 不转发 V2Ray NAT 表中访问部分地址的流量
 iptables -t nat -A V2RAY -d ${VPN_IP}/24 -j RETURN
 iptables -t nat -A V2RAY -d 119.29.29.29/24 -j RETURN
